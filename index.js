e6c6b59b8e787bf36bf83a1c3c3ecbb802152ceb
kapoo360 <az805555@gmail.com>
2024-09-23 15:05:12 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("btn2").addEventListener("click", function() {
+    document.getElementById("hero").scrollIntoView({ behavior: 'smooth' });
+  });
---
+++ topic2.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", function() {
+  document.getElementById("sec").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+  document.getElementById("hero").scrollIntoView({ behavior: 'smooth' });
+});
